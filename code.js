c7eb0b245332f33eb0d6eb2b2b7b4351119d8928
Dan Birman <danbirman@gmail.com>
2016-05-04 22:23:40 -0700

    More test code

--- code.js
+++ code.js
@@ -1,13 +1,38 @@
-$(function(){
-  $('ul.tabs li:first').addClass('active');
-  $('.block article').hide();
-  $('.block article:first').show();
-  $('ul.tabs li').on('click',function(){
-    $('ul.tabs li').removeClass('active');
-    $(this).addClass('active')
-    $('.block article').hide();
-    var activeTab = $(this).find('a').attr('href');
-    $(activeTab).show();
-    return false;
-  });
-})
+window.onload=function() {
+
+  // get tab container
+  var container = document.getElementById("tabContainer");
+    // set current tab
+    var navitem = container.querySelector(".tabs ul li");
+    //store which tab we are on
+    var ident = navitem.id.split("_")[1];
+    navitem.parentNode.setAttribute("data-current",ident);
+    //set current tab with class of activetabheader
+    navitem.setAttribute("class","tabActiveHeader");
+
+    //hide two tab contents we don't need
+    var pages = container.querySelectorAll(".tabpage");
+    for (var i = 1; i < pages.length; i++) {
+      pages[i].style.display="none";
+    }
+
+    //this adds click event to tabs
+    var tabs = container.querySelectorAll(".tabs ul li");
+    for (var i = 0; i < tabs.length; i++) {
+      tabs[i].onclick=displayPage;
+    }
+}
+
+// on click of one of tabs
+function displayPage() {
+  var current = this.parentNode.getAttribute("data-current");
+  //remove class of activetabheader and hide old contents
+  document.getElementById("tabHeader_" + current).removeAttribute("class");
+  document.getElementById("tabpage_" + current).style.display="none";
+
+  var ident = this.id.split("_")[1];
+  //add class of activetabheader to new active tab and show contents
+  this.setAttribute("class","tabActiveHeader");
+  document.getElementById("tabpage_" + ident).style.display="block";
+  this.parentNode.setAttribute("data-current",ident);
+}
