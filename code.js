1979eeb937199338391c8080051e6880f7d5b247
Dan Birman <danbirman@gmail.com>
2016-05-05 23:22:08 -0700

    Adding meal groups

--- code.js
+++ code.js
@@ -1,7 +1,7 @@
 // MAIN VARIABLES
+
+//track what is in basket:
 in_basket = []
-
-
 
 window.onload=function() {
   $("#list").append("<li>wow this works</li>");
@@ -10,8 +10,33 @@ window.onload=function() {
 }
 
 function buildPage() {
-  // get tab container
-  var container = document.getElementById("tabContainer");
+  // First deal with the meal tabs
+  var container = document.getElementById("tabContainer_meals");
+  var innercontainer = document.getElementById("innerwrapper");
+  // set current tab
+  var navitem = container.querySelector(".tabs ul li");
+  //store which tab we are on
+  ident = navitem.id.split("_")[1];
+  navitem.parentNode.setAttribute("data-current",ident);
+  //set current tab with class of activetabheader
+  navitem.setAttribute("class","tabActiveHeader");
+  //hide two tab contents we don't need
+  var pages = innercontainer.querySelectorAll(".conpage");
+  for (var i = 1; i < pages.length; i++) {
+    pages[i].style.display="none";
+  }
+
+  //this adds click event to tabs
+  var tabs = container.querySelectorAll(".tabs ul li");
+  for (var i = 0; i < tabs.length; i++) {
+    tabs[i].onclick=displayMeal;
+  }
+
+  mealgroups = [0,1,2];
+
+  for (mi in mealgroups) {
+    // get tab container
+    var container = document.getElementById("tabContainer_"+mealgroups[mi]);
     // set current tab
     var navitem = container.querySelector(".tabs ul li");
 
@@ -20,7 +45,7 @@ function buildPage() {
       return
     }
     //store which tab we are on
-    var ident = navitem.id.split("_")[1];
+    ident = navitem.id.split("_")[1];
     navitem.parentNode.setAttribute("data-current",ident);
     //set current tab with class of activetabheader
     navitem.setAttribute("class","tabActiveHeader");
@@ -36,6 +61,7 @@ function buildPage() {
     for (var i = 0; i < tabs.length; i++) {
       tabs[i].onclick=displayPage;
     }
+  }
 }
 
 function loadData() {
@@ -53,16 +79,19 @@ function buildHTML() {
     in_basket[i] = 0;
     var recipe = recipes[i];
     var cname = recipe.name;
+    var quant = recipe.quantities;
     var ings = recipe.ingredients;
+    var group = recipe.group;
     var ctext = recipe.text;
     // Add the header
-    $("#tabHeader").append("<li id=\"tabHeader_" + c + "\">" + cname + "</li>");
+    $("#tabHeader_"+group).append("<li id=\"tabHeader_" + c + "\">" + cname + "</li>");
     // Add the content page
-    $("#tabscontent").append("<div class=\"tabpage\" id=\"tabpage_"+ c + "\"> <h2>"+cname+"</h2> <ul id=\"list_"+c+"\"> </ul> <br> <p>"+ctext+"</p>  <button type=\"button\" id=\"button_"+c+"\" onclick=\"clicked("+c+");\">+</button> </div>");
+    $("#tabscontent_"+group).append("<div class=\"tabpage\" id=\"tabpage_"+ c + "\"> <h2>"+cname+"</h2> <ul id=\"list_"+c+"\"> </ul> <br> <p>"+ctext+"</p>  <button type=\"button\" id=\"button_"+c+"\" onclick=\"clicked("+c+");\">+</button> </div>");
     // Add the ingredients
     for (j in ings) {
       cing = ings[j];
-      $("#list_"+c).append("<li>1 Cup - "+ cing +"</li>")
+      qtext = metric2imperial(quant[j],"cup");
+      $("#list_"+c).append("<li>" + qtext + " " + cing +"</li>")
     }
     c++;
   }
@@ -79,20 +108,54 @@ function clicked(id) {
 function buildBasket() {
   $("#basketContent").empty();
   $("#groceryList").empty();
+  all_ings = [];
+  all_quants = [];
   for (i in in_basket) {
     if (in_basket[i]==1) {
       var recipe = recipes[i];
       $("#basketContent").append("<li>"+ recipe.name + "</li>");
       var ings = recipe.ingredients;
+      var quants = recipe.quantities;
       for (j in ings) {
-        $("#groceryList").append("<li>"+ings[j]+"</li>");
+        cing = ings[j];
+        cquant = quants[j];
+        found = false;
+        for (k in all_ings) {
+          if (all_ings[k]==cing) {
+            all_quants[k] += cquant;
+            found = true;
+          }
+        }
+        if (!found) {
+          all_ings.push(cing);
+          all_quants.push(cquant);
+        }
       }
     }
+  }
+
+  for (i in all_ings) {
+    var qtext = metric2imperial(all_quants[i],"cup");
+    $("#groceryList").append("<li>"+qtext+" "+all_ings[i]+"</li>");
   }
 }
 
 function saveData() {
   $.saveJ
+}
+
+function displayMeal() {
+  var current = this.parentNode.getAttribute("data-current");
+  //remove class of activetabheader and hide old contents
+  document.getElementById("tabHeader_" + current).removeAttribute("class");
+  document.getElementById("tabpage_" + current).style.display="none";
+
+  var ident = this.id.split("_")[1];
+  //add class of activetabheader to new active tab and show contents
+  this.setAttribute("class","tabActiveHeader");
+  document.getElementById("tabpage_" + ident).style.display="block";
+  this.parentNode.setAttribute("data-current",ident);
+
 }
 
 // on click of one of tabs
@@ -109,4 +172,37 @@ function displayPage() {
   this.parentNode.setAttribute("data-current",ident);
 }
 
+//Measures:
+//pinch = 
+//1 tsp = 5ml
+//1 tbsp = 15ml
+//1/4 cup = 60 ml
+//1/3 cup = 80 ml
+//1/2 cup = 120 ml
+//1 cup = 240 ml
+//1 pint = 480 ml
+//1 quart = 960 ml
+//1 gallon = 4*960 ml :)
+function metric2imperial(grams,maxunit) {
+  var amts = [4*960,960,480,240,15,5,1];
+  var units = ["gallon","quart","pint","cup","tbsp","tsp","pinch"]
 
+  var string = "";
+  var passed = false;
+  for (ui in units) {
+    if (units[ui]==maxunit || passed) {
+      // we start here!
+      passed = true;
+      var cur = Math.floor(grams / amts[ui]);
+      var grams = grams % amts[ui];
+      if (cur > 0) {
+        if (cur > 1) {
+          string = string + " " + cur + " " + units[ui] + "s";
+        } else {
+          string = string + " " + cur + " " + units[ui];
+        }
+      }
+    }
+  }
+  return string;
+}
